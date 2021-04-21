551c128d160213279cd5cd7ee0e96c8683e60f6a
Paul Nodet <5941125+pnxdxt@users.noreply.github.com>
2021-04-21 02:59:53 +0200

    FIX: Update usage config

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 module.exports = {
-  arrowParens: 'avoid',
-  bracketSpacing: false,
-  jsxBracketSameLine: false,
-  jsxSingleQuote: false,
-  printWidth: 80,
-  proseWrap: 'always',
+  // Only quote properties that are invalid identifiers.
   quoteProps: 'as-needed',
-  semi: true,
+  // Use single quotes instead of double quotes.
   singleQuote: true,
+  // Remove parentheses around a sole arrow function parameter.
+  arrowParens: 'avoid',
+  // Use soft tabs (space character) set to 2 spaces.
   tabWidth: 2,
-  trailingComma: 'es5',
   useTabs: false,
+  // Wrap prose if it exceeds the print width.
+  proseWrap: 'always',
+  // Specify the line length that the printer will wrap on.
+  printWidth: 80,
+  // Do not print spaces between brackets in object literals.
+  bracketSpacing: false,
+  // Print trailing commas wherever possible when multi-line.
+  trailingComma: 'es5',
+  // Print semicolons at the ends of statements.
+  semi: true,
+  // Use double quotes in JSX.
+  jsxSingleQuote: false,
+  // If your component has multi-line properties, close its tag on a new line.
+  jsxBracketSameLine: false,
 };
